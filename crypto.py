109cdb3009610b451b7812ecebb03422af0f12db
mdisant <59811474+mdisant@users.noreply.github.com>
2021-12-05 19:13:19 -0500

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -2,3 +2,19 @@
 
 # test
 
+SECRET_KEY = "sk_303754f6818844f9a85f1e9203a48b0b"
+PUBLIC_KEY = "pk_0d7f6caccf0f42498aeb1bc2fc4d915f"
+
+def get_crpyto_data(c1, c2, c3):
+
+    pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
