d8ad1e460d56924fef4e400558108d71da523561
mdisant <mdisant@umich.edu>
2021-12-06 20:43:29 -0500

    Databases are done

--- twitter.py
+++ twitter.py
@@ -7,11 +7,18 @@ import sqlite3
 import os
 import re
 
-consumer_key = "tv6h21bxvLZe4EfUKb6jEOngO"
-consumer_secret = "xHwJCd0Km4JqjxxzJlgfPYMtkbMGKvKQVdQfZb7hQYcMddRMGP"
-access_token = "2967918434-PBT2EGFDYnhEABVox00z6hk8zxlbkIO1kmO48uy"
-access_token_secret = "gWltdO8LBAfOtfv7X5s3BZvnhCpKkpisJAZD4q3taQqgh"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAPV4VwEAAAAABNBqe8IOkS6pIaOf%2Fn65R%2BH00k0%3DEAttM3EYnstTxcHXx2mNzrapL6g1xMQYbZVumRriagVMXDXtS4"
+# consumer_key = "tv6h21bxvLZe4EfUKb6jEOngO"
+# consumer_secret = "xHwJCd0Km4JqjxxzJlgfPYMtkbMGKvKQVdQfZb7hQYcMddRMGP"
+# access_token = "2967918434-PBT2EGFDYnhEABVox00z6hk8zxlbkIO1kmO48uy"
+# access_token_secret = "gWltdO8LBAfOtfv7X5s3BZvnhCpKkpisJAZD4q3taQqgh"
+# bearer_token = "AAAAAAAAAAAAAAAAAAAAAPV4VwEAAAAABNBqe8IOkS6pIaOf%2Fn65R%2BH00k0%3DEAttM3EYnstTxcHXx2mNzrapL6g1xMQYbZVumRriagVMXDXtS4"
+
+consumer_key = "TfyjYQDGNhNm7KOuQa2JQUZ6x"
+consumer_secret = "5YetECIama1can1SwkRmEaXBmTz8KnXofqImlOH7Pfbb0IFEbO"
+access_token = "1400081139361013762-qJFq7xtQ4viWUNwN33tFtF5D8vIAdu"
+access_token_secret = "aYN0nlJqEfp86Yk24S1U7q65eePt3OlyW9hbesX15eveZ"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAANtPWgEAAAAAkoStv0CJRLz57bfTtfMgLgWOx1w%3DOejMKa67emE79o4sBWHJbolAPvRr0uGsCff1AcMOK13NmZpBtE"
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret, return_type= dict)
@@ -29,15 +36,9 @@ def create_twitter_tuple(cur, conn):
 
         # search for the crypto in Twitter and grab the results from the search
         results = api.get_recent_tweets_count(query=crypto)
-        # start_list = []
-        # end_list = []
-        # count_list = []
 
         results_list = results['data']
         for dict in results_list:
-            # start_list.append(dict["start"])
-            # end_list.append(dict["end"])
-            # count_list.append(dict["tweet_count"])
             tuple_list.append((crypto[0], dict["start"], dict["end"], dict["tweet_count"]))
     
     return tuple_list
@@ -63,12 +64,29 @@ def setUpTweetsTable(tupleslist, cur, conn):
     
     conn.commit()
 
+def combined_data(cur):
+    cur.execute("SELECT * FROM Tweets JOIN Crypto ON Tweets.crypto = Crypto.name")
+    data = cur.fetchall()
+    return data
+
+def make_combined_table(data, cur, conn):
+    cur.execute("DROP TABLE IF EXISTS Combined")
+    cur.execute("CREATE TABLE IF NOT EXISTS Combined (crypto_name TEXT PRIMARY KEY, count INTEGER, increase_24h REAL, increase_7d REAL)")
+    
+    for tup in data:
+
+        cur.execute("INSERT OR IGNORE INTO Combined (crypto_name,count, increase_24h, increase_7d) VALUES (?,?,?,?)",(tup[0], tup[3], tup[8], tup[9]))
+    
+    conn.commit()
+
 def main():
 
     cur,conn = setUpDatabase('crypto.db')
     result = create_twitter_tuple(cur, conn)
 
     setUpTweetsTable(result, cur, conn)
+    combined = combined_data(cur)
+    make_combined_table(combined, cur, conn)
 
     conn.close()
 
