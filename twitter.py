078372ec5c9c04e4d1fa495f2039041becdb2d8b
mdisant <mdisant@umich.edu>
2021-12-12 15:22:03 -0500

    Updated twitter file.

--- twitter.py
+++ twitter.py
@@ -6,12 +6,7 @@ import tweepy
 import sqlite3          
 import os
 import re
-
-# consumer_key = "tv6h21bxvLZe4EfUKb6jEOngO"
-# consumer_secret = "xHwJCd0Km4JqjxxzJlgfPYMtkbMGKvKQVdQfZb7hQYcMddRMGP"
-# access_token = "2967918434-PBT2EGFDYnhEABVox00z6hk8zxlbkIO1kmO48uy"
-# access_token_secret = "gWltdO8LBAfOtfv7X5s3BZvnhCpKkpisJAZD4q3taQqgh"
-# bearer_token = "AAAAAAAAAAAAAAAAAAAAAPV4VwEAAAAABNBqe8IOkS6pIaOf%2Fn65R%2BH00k0%3DEAttM3EYnstTxcHXx2mNzrapL6g1xMQYbZVumRriagVMXDXtS4"
+import numpy as np
 
 consumer_key = "TfyjYQDGNhNm7KOuQa2JQUZ6x"
 consumer_secret = "5YetECIama1can1SwkRmEaXBmTz8KnXofqImlOH7Pfbb0IFEbO"
@@ -23,23 +18,17 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.Client(bearer_token=bearer_token, consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret, return_type= dict)
     
-
-def create_twitter_tuple(cur, conn):
-    
-    # grab crypto names from the Crypto table
-    cur.execute('SELECT name FROM Crypto')
+def create_twitter_tuple(cur):
+    cur.execute('SELECT name, crypto_id FROM Crypto')
     crypto_list = cur.fetchall()
 
     tuple_list = []
-    # goes through the cryptos from Crypto table
     for crypto in crypto_list:
-
-        # search for the crypto in Twitter and grab the results from the search
-        results = api.get_recent_tweets_count(query=crypto)
+        results = api.get_recent_tweets_count(query=crypto[0])
 
         results_list = results['data']
         for dict in results_list:
-            tuple_list.append((crypto[0], dict["start"], dict["end"], dict["tweet_count"]))
+            tuple_list.append((crypto[1], dict["tweet_count"]))
     
     return tuple_list
 
@@ -50,52 +39,57 @@ def setUpDatabase(db_name):
 
     return cur, conn
 
-# 3 cols, primary id (which crypto), tweet id, and text
-
-def setUpTweetsTable(tupleslist, cur, conn):
-
+def setUpTweetsTable(tuple_list, cur, conn):
     cur.execute("DROP TABLE IF EXISTS Tweets")
-    cur.execute("CREATE TABLE IF NOT EXISTS Tweets (crypto TEXT PRIMARY KEY, start TEXT, end TEXT, count  INTEGER)")
+    cur.execute("CREATE TABLE Tweets (id INTEGER PRIMARY KEY, tweet_count INTEGER)")
     
-    # ensures 25 entries at a time, run code 4 times
-    for tup in tupleslist:
-
-        cur.execute("INSERT OR IGNORE INTO Tweets (crypto, start, end, count) VALUES (?,?,?,?)",(tup[0], tup[1], tup[2], tup[3]))
+    for tup in tuple_list:
+        cur.execute("INSERT OR IGNORE INTO Tweets (id, tweet_count) VALUES (?,?)",(tup[0], tup[1]))
     
     conn.commit()
 
-def combined_data(cur):
-    cur.execute("SELECT * FROM Tweets JOIN Crypto ON Tweets.crypto = Crypto.name")
-    data = cur.fetchall()
-    return data
+def calculate_correlation(cur, timeline):
+    cur.execute(f"SELECT Tweets.tweet_count, Crypto.{timeline} from Tweets JOIN Crypto ON Tweets.id = Crypto.crypto_id")
+    corr_data = cur.fetchall()
 
-def make_combined_table(data, cur, conn):
-    cur.execute("DROP TABLE IF EXISTS Combined")
-    cur.execute("CREATE TABLE IF NOT EXISTS Combined (crypto_name TEXT PRIMARY KEY, count INTEGER, increase_24h REAL, increase_7d REAL)")
-    
-    for tup in data:
+    xlist = [tup[0] for tup in corr_data]
+    ylist = [tup[1] for tup in corr_data]
 
-        cur.execute("INSERT OR IGNORE INTO Combined (crypto_name,count, increase_24h, increase_7d) VALUES (?,?,?,?)",(tup[0], tup[3], tup[8], tup[9]))
-    
-    conn.commit()
+    x_avg = sum(xlist) / len(xlist)
+    y_avg = sum(ylist) / len(ylist)
+
+    cor_numerator = 0
+    cor_x_denominator = 0
+    cor_y_denominator = 0
+
+    for i in range(len(xlist)):
+        x_dif = xlist[i] - x_avg
+        y_dif = ylist[i] - y_avg
+        
+        cor_numerator += x_dif * y_dif
+        cor_x_denominator += x_dif ** 2
+        cor_y_denominator += y_dif ** 2
+
+    corr = cor_numerator / np.sqrt(cor_x_denominator * cor_y_denominator)
+    return round(corr, 5)
 
 def write_out_data(fname, cur):
-    with open(fname, "w") as f:
-        cur.execute("SELECT * FROM Combined")
-        for data in cur:
-            w_str = f"{data[0]} has {data[1]} recent tweets and the price has changed {data[2]}% in the past 24h and {data[3]}% in the past 7 days.\n"
-            f.write(w_str)
+    correlation_24h = calculate_correlation(cur, "percent_change_7d")
+    correlation_7d = calculate_correlation(cur, "percent_change_24h")
 
+    with open(fname, "w") as f:
+        f.write(f"The calculated Pearson Correlation Coefficient between number of Tweets and percent change in the past 24 hours is:\n{correlation_24h}\n")
+        f.write("\n")
+        f.write(f"The calculated Pearson Correlation Coefficient between number of Tweets and percent change in the past 7 days is:\n{correlation_7d}\n")
 
 def main():
-
     cur,conn = setUpDatabase('crypto.db')
-    result = create_twitter_tuple(cur, conn)
 
+    result = create_twitter_tuple(cur)
     setUpTweetsTable(result, cur, conn)
-    combined = combined_data(cur)
-    make_combined_table(combined, cur, conn)
+
     write_out_data("twitter.txt", cur)
+
     conn.close()
 
 if __name__ == "__main__":
